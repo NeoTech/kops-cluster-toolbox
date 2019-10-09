2eac690196b8d4486ff9d8c118d77857895865cc
NeoTech <andreas@roughedge.se>
2019-10-09 21:46:12 +0200

    Tidied up the output to be more kops friendly and made backend init more flexible.

--- terraform/config.tf
+++ terraform/config.tf
@@ -3,8 +3,6 @@ terraform {
 
   // See https://github.com/hashicorp/terraform/issues/13022
   backend "s3" {
-    key = "terraform.tfstate"
-    bucket = "ops.k8s.nearform.net-tf-state"
-    region = "eu-central-1"
+    key    = "terraform.tfstate"
   }
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,10 @@
-output "private_key" {
+output "private-key" {
   value = tls_private_key.kops-key.private_key_pem
 }
-output "kops_id" {
+output "kops-id" {
   value = aws_iam_access_key.kops_access_key.id
 }
-output "kops_secret" {
+output "kops-secret" {
   value = aws_iam_access_key.kops_access_key.secret
 }
 output "build-toolbox-ecr" {
@@ -13,11 +13,37 @@ output "build-toolbox-ecr" {
 output "vpc-id" {
   value = module.vpc.vpc_id
 }
-output "private-subnets" {
-  value = module.vpc.private_subnets
+output "public-nets" {
+  value = <<EOT
+- cidr: ${module.vpc.public_subnets_cidr_blocks[0]}
+  name: ${var.project_name}-public-${var.aws_region}a
+  type: Utility
+  id: ${module.vpc.public_subnets[0]}
+- cidr: ${module.vpc.public_subnets_cidr_blocks[1]}
+  name: ${var.project_name}-public-${var.aws_region}b
+  type: Utility
+  id: ${module.vpc.public_subnets[1]}
+- cidr: ${module.vpc.public_subnets_cidr_blocks[2]}
+  name: ${var.project_name}-public-${var.aws_region}c
+  type: Utility
+  id: ${module.vpc.public_subnets[2]}
+EOT
 }
-output "public-subnets" {
-  value = module.vpc.public_subnets
+output "private-nets" {
+  value = <<EOT
+- cidr: ${module.vpc.private_subnets_cidr_blocks[0]}
+  name: ${var.project_name}-private-${var.aws_region}a
+  type: Private
+  id: ${module.vpc.private_subnets[0]}
+- cidr: ${module.vpc.private_subnets_cidr_blocks[1]}
+  name: ${var.project_name}-private-${var.aws_region}b
+  type: Private
+  id: ${module.vpc.private_subnets[1]}
+- cidr: ${module.vpc.private_subnets_cidr_blocks[2]}
+  name: ${var.project_name}-private-${var.aws_region}c
+  type: Private
+  id: ${module.vpc.private_subnets[2]}
+EOT
 }
 output "aws-region" {
   value = var.aws_region
