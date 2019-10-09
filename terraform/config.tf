c3196d3202c9e122dcf58d25ab922e3c97349be4
NeoTech <andreas@roughedge.se>
2019-10-09 16:39:24 +0200

    Cant add these variables from cli

--- terraform/config.tf
+++ terraform/config.tf
@@ -4,5 +4,7 @@ terraform {
   // See https://github.com/hashicorp/terraform/issues/13022
   backend "s3" {
     key = "terraform.tfstate"
+    bucket = "ops.k8s.nearform.net-tf-state"
+    region = "eu-central-1"
   }
 }
